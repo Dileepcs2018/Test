a7332645e0244acef55fb36589e0fc9992e68d93
Dileepcs2018 <34806249+Dileepcs2018@users.noreply.github.com>
2020-04-11 16:27:33 +0530

    Add files via upload

---
+++ substring_in_string.py
@@ -0,0 +1,21 @@
+str = "welcome to india"
+substr = "india"
+new_str = str.split(" ")
+flag =0
+for s in new_str:
+    if s != substr:
+        pass
+    else:
+        print("string is present")
+
+
+   
+
+
+
+
+
+
+
+
+
